94cc8f81219d71753347a431cad57a926cbac9c0
ParkChan <hydrarkk@naver.com>
2020-05-16 03:27:05 +0900

    [작업내용] 통신모듈 초안 작업

---
+++ app/src/main/java/com/examsample/ui/bookmark/BookmarkFragment.kt
@@ -0,0 +1,31 @@
+package com.examsample.ui.bookmark
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.examsample.R
+
+class BookmarkFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: BookmarkViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(BookmarkViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_bookmark, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
