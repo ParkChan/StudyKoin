1082836b8077643cb2b54585bb43f44ea039e6d6
ParkChan <hydrarkk@naver.com>
2020-05-18 01:38:36 +0900

    [작업내용] 북마크 즐겨 찾기 삭제 처리

--- app/src/main/java/com/examsample/ui/bookmark/adapter/BindingBookmarkAdapter.kt
+++ app/src/main/java/com/examsample/ui/bookmark/adapter/BindingBookmarkAdapter.kt
@@ -1,14 +1,8 @@
 package com.examsample.ui.bookmark.adapter
 
-import android.view.View
-import android.widget.Toast
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.examsample.ui.bookmark.local.BookmarkDatabase
 import com.examsample.ui.bookmark.model.BookmarkModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 
 @BindingAdapter("bookMarkListData")
@@ -16,19 +10,6 @@ fun setBookMarkListData(recyclerView: RecyclerView, items: List<BookmarkModel>?)
     items?.let {
         if (recyclerView.adapter is BookmarkAdapter) {
             (recyclerView.adapter as BookmarkAdapter).submitList(items)
-        }
-    }
-}
-
-@BindingAdapter("deleteBookmark")
-fun deleteBookmark(view: View, model: BookmarkModel?) {
-    view.setOnClickListener {
-        model?.let {
-            Toast.makeText(view.context, "${model.name} delete bookmark!!", Toast.LENGTH_SHORT)
-                .show()
-            CoroutineScope(Dispatchers.IO).launch {
-                BookmarkDatabase.getInstance(view.context).bookmarkDao().delete(model)
-            }
         }
     }
 }
