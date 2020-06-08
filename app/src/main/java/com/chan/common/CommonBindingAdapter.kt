83b0b394cfe9f41038bc4ccd8c5d04f3122fbbd0
ParkChan <hydrarkk@naver.com>
2020-06-09 01:51:39 +0900

    [작업내용] BaseAdapter 클래스 이름 수정 및 Dagger2 적용을 위한 gradle 추가

--- app/src/main/java/com/chan/common/CommonBindingAdapter.kt
+++ app/src/main/java/com/chan/common/CommonBindingAdapter.kt
@@ -7,7 +7,7 @@ import androidx.databinding.BindingAdapter
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.chan.common.base.BaseAdapter
+import com.chan.common.base.BaseRecyclerAdapter
 import com.chan.ui.detail.ProductDetailViewModel
 import com.chan.ui.home.model.ProductModel
 import com.chan.ui.home.viewmodel.HomeViewModel
@@ -16,7 +16,7 @@ import java.text.DecimalFormat
 @Suppress("UNCHECKED_CAST")
 @BindingAdapter("addAllItems")
 fun addAllItems(recyclerView: RecyclerView, items: List<Any>?) {
-    (recyclerView.adapter as? BaseAdapter<Any>)?.run {
+    (recyclerView.adapter as? BaseRecyclerAdapter<Any>)?.run {
         items?.let { addAllItems(it) }
     }
 }
